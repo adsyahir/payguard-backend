70ee6a6f368988439159b69f6c1c00e512e8a368
adsyahir <adsyahir16@gmail.com>
2026-07-19 21:59:43 +0800

    Initial commit

---
+++ infra/postgres-init.sql
@@ -0,0 +1,6 @@
+-- One database per service (database-per-service pattern, single local instance).
+CREATE DATABASE userdb;
+CREATE DATABASE paymentdb;
+CREATE DATABASE frauddb;
+CREATE DATABASE notificationdb;
+CREATE DATABASE reconciliationdb;
